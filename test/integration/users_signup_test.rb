ce9c4d6649c3abe13f08569475c776dcb2888f96
k.sasaki <br3.kyokyo@gmail.com>
2021-12-25 21:03:08 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,6 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
 
     assert !flash.nil?
     assert_select 'span','Welcome to the Sample App!'
